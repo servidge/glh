03a7f420554724e062fc9bb4b33b809d0a3443e6
Sebastian <news.sb@web.de>
2018-02-09 17:52:58 +0100

    TCP, UDP, ICMP
    teilweise umgesetzt

--- glh.sh
+++ glh.sh
@@ -9,6 +9,8 @@ OIFS=$IFS
 STARTSPALTE=8
 SPALTEQUEL=9
 SPALTEZIEL=10
+PROTOSPAL=0
+TYPESPAL=0
 IFS=' '
 
 FILE=$1
@@ -41,11 +43,45 @@ do
 						ausgabe=$((0x${SPALTE:0:2})).$((0x${SPALTE:2:2})).$((0x${SPALTE:4:2})).$((0x${SPALTE:6:2}))" "$((${SPALTE:9:$spaltlen-10}))
 						OUTPUT=$OUTPUT" "$ausgabe 
 				fi
+				if [[ $SPALTE = "proto" ]]; then
+					PROTOSPAL=$((counter+1))
+				fi
+				if [[ ${counter} -eq $PROTOSPAL ]]; then
+					if [[ $SPALTE -eq "1" ]]; then
+						OUTPUT="ICMP "$OUTPUT
+					elif [[ $SPALTE -eq "6" ]]; then
+						OUTPUT="TCP "$OUTPUT
+					elif [[ $SPALTE -eq "17" ]]; then
+						OUTPUT="UDP "$OUTPUT
+					elif  [[ $SPALTE -eq "50" ]]; then
+						OUTPUT="ESP "$OUTPUT
+					elif [[ $SPALTE -eq "51" ]]; then
+						OUTPUT="AH "$OUTPUT
+					else
+						eOUTPUT="UNBEKANNTESproto "$OUTPUT
+					fi
+				fi
+				if [[ $SPALTE = "type" ]]; then
+				        TYPESPAL=$((counter+1))
+				fi 
+				if [[ ${counter} -eq $TYPESPAL ]]; then
+					if [[ $SPALTE -eq "8" ]]; then
+						OUTPUT=$OUTPUT" ""ping"
+					elif [[ $SPALTE -eq "0" ]]; then
+						OUTPUT=$OUTPUT" ""reply"
+					elif [[ $SPALTE -eq "3" ]]; then
+						OUTPUT=$OUTPUT" ""unreeach "
+					else
+						OUTPUT=$OUTPUT" ""type??"
+					fi
+				fi 
 			fi
 			((counter++))
 		done
 	fi
-[ "$OUTPUT" ] && echo $OUTPUT
+PROTOSPAL=0
+TYPESPAL=0
+[ "$OUTPUT" ] && echo ipfw_log" "$OUTPUT
 OUTPUT=""
 done < $FILE
 
